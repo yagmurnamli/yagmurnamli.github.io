01fe15452280fd66b3c2f112727fa1d72a5c909b
yagmurnamli <75357759+yagmurnamli@users.noreply.github.com>
2026-03-22 21:43:32 +0300

    Delete nodes directory

--- nodes/node5/noise.js
+++
@@ -1,7 +0,0 @@
-var paragraph = document.getElementById('clickableParagraph');
-var soundEffect = document.getElementById('soundEffect');
-
-        // Ajouter un événement de clic sur la balise <p>
-        paragraph.addEventListener('click', function() {
-            soundEffect.play();
-        });
