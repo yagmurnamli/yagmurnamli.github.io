696692d0fecd65854e30632a0cbcae6992495871
yagmurnamli <75357759+yagmurnamli@users.noreply.github.com>
2026-03-22 21:32:04 +0300

    Delete theMaze directory

--- theMaze/nodes/node4-3/change.js
+++
@@ -1,7 +0,0 @@
-function Changeeye() {
-    document.getElementById("puppet").src = "EXPRESSION3.png";
-}
-
-function Changemouse() {
-    document.getElementById("puppet").src = "EXPRESSION2.png";
-}
