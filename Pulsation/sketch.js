d6601f2747d0bdc3802b4fa772abc8ecd4f4c60b
Yağmur Namlı <75357759+yagmurnamli@users.noreply.github.com>
2026-03-27 02:10:28 +0300

    Update sketch.js

--- Pulsation/sketch.js
+++ Pulsation/sketch.js
@@ -44,7 +44,7 @@ void main() {
     float dx = xs[i] - vPos.x;
     float dy = ys[i] - vPos.y;
     float d = length(vec2(dx, dy));
-    sum += rs[i] / d;
+    sum += (rs[i] * 0.2) / d;
   }
 
   if (sum > 11.0) {
